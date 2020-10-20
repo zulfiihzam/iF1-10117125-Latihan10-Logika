6367ccb25d01411023ccfd8a6852395a6cb4a2f6
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-21 00:50:24 +0700

    Add files via upload

---
+++ iF1-10117125-Latihan10-Logika/src/if1/pkg10117125/latihan10/logika/IF110117125Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117125.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * Nama  : Zulfi Ihzam Rahmat
+ * Kelas : IF 1
+ * NIM   : 10117125
+ * Deskripsi Program : 
+ */
+public class IF110117125Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
